9e9053c254a5a8958b4b652cd261c951dafa41e6
chrissyzic <chrissy.ziccarelli@gmail.com>
2015-01-12 21:29:38 -0500

    Create ex34.py

---
+++ ex34.py
@@ -0,0 +1,10 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# 1. The animal at 1 is the second animal and is a python.
+# 2. The third animal is a at 2 and is a peacock.
+# 3. The first animal is at 0 and is a bear.
+# 4. The animal at 3 is the fourth animal and is a kangaroo.
+# 5. The fifth animal is at 4 and is a whale.
+# 6. The animal at 2 is the third animal and is a peacock
+# 7. The sixth animal is at 5 and is a platypus
+# 8. The animal at 4 is the fifth animal and is a whale.
